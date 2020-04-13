4c8cc08b44d10522adac577462b538cc8b5cc459
BeagleBone Black <debian@beaglebone>
2020-04-13 15:18:44 +0000

    Add .gitignore

--- .my_ds1307.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/debian/ds1307-driver/my_ds1307.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/debian/ds1307-driver/my_ds1307.ko /home/debian/ds1307-driver/my_ds1307.o /home/debian/ds1307-driver/my_ds1307.mod.o
--- .tmp_versions/my_ds1307.mod
+++
@@ -1,2 +0,0 @@
-/home/debian/ds1307-driver/my_ds1307.ko
-/home/debian/ds1307-driver/my_ds1307.o
